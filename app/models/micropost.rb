032a98572761e3cfb45533316de2fd57d60490da
nonoykt <nonoykt@gmail.com>
2020-06-23 08:22:55 +0900

    update models user, micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   mount_uploader :image, ImageUploader
+  mount_base64_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :microposts, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :username, presence: true, length: { maximum: 32 }
+  validates :email, length: { maximum: 255 }
 
   mount_uploader :image, ImageUploader
+  mount_base64_uploader :image, ImageUploader
+
 end
